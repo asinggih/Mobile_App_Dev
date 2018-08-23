c204f21a2069896a0170326d28ba835569695c2f
Aditya Singgih <adityasinggih@msn.com>
2018-08-23 12:23:18 +1000

    added AddNewExam activity

---
+++ Student_management_app/app/src/main/java/blob/happypetsy/studentmanagementportal/Add_Exam.java
@@ -0,0 +1,19 @@
+package blob.happypetsy.studentmanagementportal;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Add_Exam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_add__exam);
+
+    }
+
+}
