fb5952a36a0b732c3fa329523f6fd4b95d930414
Aditya Singgih <adityasinggih@msn.com>
2018-08-31 22:42:50 +1000

    Exam datePicker finished

--- Student_management_app/app/src/main/java/blob/happypetsy/studentmanagementportal/Add_Exam.java
+++ Student_management_app/app/src/main/java/blob/happypetsy/studentmanagementportal/Add_Exam.java
@@ -1,5 +1,7 @@
 package blob.happypetsy.studentmanagementportal;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -7,16 +9,84 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
 
 public class Add_Exam extends AppCompatActivity {
 
     Button closeBut;
+    Spinner durationDropdown;
+    EditText examNameInput, examLocInput, examDateInput;
+    Calendar calendar;
+    int day, month, year;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_add__exam);
+
+        calendar = Calendar.getInstance();
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        month = calendar.get(Calendar.MONTH);
+        year = calendar.get(Calendar.YEAR);
+
+        examDateInput = findViewById(R.id.exam_date_input);
+        examDateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(Add_Exam.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker dp, int year, int month, int day) {
+                                Calendar examCalendar = Calendar.getInstance();
+                                examCalendar.set(year,month, day);
+                                examDateInput.setText(new SimpleDateFormat("EEEE, dd/MM/yyyy").format(examCalendar.getTime()));
+                            }
+                        }, year, month, day);
+                datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("1 hour");
+        categories.add("2 hours");
+        categories.add("3 hours");
+
+        durationDropdown = (Spinner) findViewById(R.id.exam_duration);
+        durationDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        durationDropdown.setAdapter(dataAdapter);
+
 
         closeBut = (Button) findViewById(R.id.newExam_close_button);
         closeBut.setOnClickListener(new View.OnClickListener() {
@@ -27,8 +97,10 @@ public class Add_Exam extends AppCompatActivity {
             }
         });
 
-
-
     }
+
+
+
+
 
 }
