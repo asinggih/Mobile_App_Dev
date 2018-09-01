b4736f100f736cf9dc187430b4b4ab18b32a07ce
Aditya Singgih <adityasinggih@msn.com>
2018-09-02 00:51:15 +1000

    Created Exam Details class

---
+++ Student_management_app/app/src/main/java/ExamDetails.java
@@ -0,0 +1,41 @@
+
+
+package blob.happypetsy.studentmanagementportal;
+
+
+import java.util.Calendar;
+
+public class ExamDetails {
+
+    private String name, location, startTime;
+    private Calendar date;
+    private int duration;
+
+    public ExamDetails(String name, String location, Calendar date, String startTime, int duration){
+        this.name = name;
+        this.location = location;
+        this.date = date;
+        this.startTime = startTime;
+        this.duration = duration;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public Calendar getDate() {
+        return this.date;
+    }
+
+    public String getStartTime() {
+        return this.startTime;
+    }
+
+    public int getDuration(){
+        return this.duration;
+    }
+}
