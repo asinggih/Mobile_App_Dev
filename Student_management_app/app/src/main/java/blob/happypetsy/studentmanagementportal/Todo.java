f42cd499ec50f0cfce9413087a6464572653ed31
Aditya Singgih <adityasinggih@msn.com>
2018-09-02 22:58:20 +1000

    Finished "add new task" UI

--- Student_management_app/app/src/main/java/blob/happypetsy/studentmanagementportal/Todo.java
+++ Student_management_app/app/src/main/java/blob/happypetsy/studentmanagementportal/Todo.java
@@ -5,15 +5,54 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class Todo extends AppCompatActivity {
+
+    Button newTaskBut;
+    RelativeLayout newTaskContainer;
+    CardView newTaskCard;
+    TextView cancelLink, addLink;
+    blob.happypetsy.studentmanagementportal.Task task;
+
+    boolean visible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_todo);
+
+        newTaskContainer = (RelativeLayout) findViewById(R.id.new_task_container);
+        newTaskCard = (CardView) findViewById(R.id.new_task_card);
+
+        newTaskBut = (Button) findViewById(R.id.newTask_button);
+        newTaskBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newTaskCard.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        cancelLink = (TextView) findViewById(R.id.cancel_task_link);
+        cancelLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newTaskCard.setVisibility(View.GONE);
+
+            }
+        });
 
         TextView title = (TextView) findViewById(R.id.title_todo);
         title.setText("Todo activity");
