aee1d492d682a86a5ac0077f80e6827e84318583
Aditya Singgih <adityasinggih@msn.com>
2018-10-05 10:48:51 +1000

    exam edeletion is finished

--- SMA_iOS/Student Management/Student Management/examListVC.swift
+++ SMA_iOS/Student Management/Student Management/examListVC.swift
@@ -71,6 +71,69 @@ class examListVC: UIViewController {
         toggleEdittingButtons()
     }
     
+    @IBAction func selectAllExams(_ sender: Any) {
+        for section in 0..<examTableView.numberOfSections {
+            for row in 0..<examTableView.numberOfRows(inSection: section) {
+                examTableView.selectRow(at: IndexPath(row: row, section: section), animated: false, scrollPosition: .none)
+            }
+        }
+    }
+    
+    
+    @IBAction func deleteSelection(_ sender: Any) {
+        let selected_rows =  examTableView.indexPathsForSelectedRows ?? []
+        
+        if selected_rows.count > 0{
+            /* NOTE:
+             Array needs to be reversed so that it deletes
+             the item with higher index first. Otherwise the list
+             will go out of range since the index gets smaller and smaller
+             with the removal
+             */
+            let alert = generateAlert(selected_rows.count, selected_rows)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    func generateAlert(_ examsToBeDeleted:Int, _ toBeDeletedArray:[IndexPath]) -> UIAlertController{
+        let numOfExams = String(examsToBeDeleted)
+        let message = "You are about to delete \(numOfExams) exams"
+        
+        let alert = UIAlertController(title: "Are you sure ?",
+                                      message: message,
+                                      preferredStyle: UIAlertControllerStyle.alert)
+        
+        let okButton = UIAlertAction(title: "Yes",
+                                     style: UIAlertActionStyle.default,
+                                     handler: {action in self.comboDelete(toBeDeletedArray)})
+        
+        let cancelButton = UIAlertAction(title: "No",
+                                         style: UIAlertActionStyle.default,
+                                         handler: nil)
+        
+        alert.addAction(cancelButton)
+        alert.addAction(okButton)
+        
+        return alert
+    }
+    
+    func comboDelete(_ itemsToBeDeleted:[IndexPath]) {
+        
+        let appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+        
+        for indexPath in itemsToBeDeleted.reversed(){
+            context.delete(examList[indexPath.row])
+            self.examList.remove(at: indexPath.row)
+            examTableView.deleteRows(at: [indexPath], with: .fade)
+        }
+        
+        appDelegate.saveContext()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -103,29 +166,6 @@ class examListVC: UIViewController {
         }
     }
     
-    private func toggleItem() {
-//        if self.studentsTableView.isEditing == true{
-//            cancelEdit.title = "Cancel"
-//            selectAll.title = "Select all"
-//            addStudent.title = ""
-//            delSelection.isHidden = false
-//            studentController.isEnabled = false
-//            examContoller.isEnabled = false
-//
-//        }
-//        else{
-//            cancelEdit.title = ""
-//            selectAll.title = ""
-//            addStudent.title = "Add student"
-//            delSelection.isHidden = true
-//            studentController.isEnabled = true
-//            examContoller.isEnabled = true
-//        }
-        
-       
-    }
-
-
 }
 
 
@@ -151,6 +191,20 @@ extension examListVC: UITableViewDataSource{
         examCell.examLocation.text = exam.location
         
         return examCell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        let appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+        
+        if editingStyle == .delete{
+            context.delete(examList[indexPath.row])  // delete from Core Data
+            self.examList.remove(at: indexPath.row)  // delete from list
+            examTableView.deleteRows(at: [indexPath], with: .fade) // delete from table
+            
+            appDelegate.saveContext() // commit the deletion
+            
+        }
     }
 
 
